c2348d996c998401d07a489681197f66797fad05
Joshua Germon <joshgermon@gmail.com>
2022-08-14 22:31:21 +1000

    Sass & Svelte added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
 import { defineConfig } from 'astro/config';
 import fonts from 'astro-fonts-next';
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        fonts({ url: 'https://fonts.googleapis.com/css2?family=Manrope&family=Space+Grotesk:wght@500&display=swap' })
-    ],
-    vite: {
-        ssr: {
-            external: ["svgo"],
-        },
-    },
-    experimental: {
-        integrations: true,
-    },
+  integrations: [fonts({
+    url: 'https://fonts.googleapis.com/css2?family=Manrope&family=Space+Grotesk:wght@500&display=swap'
+  }), svelte()],
+  vite: {
+    ssr: {
+      external: ["svgo"]
+    }
+  },
+  experimental: {
+    integrations: true
+  }
 });
