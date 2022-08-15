c1a2e5b83074b68f6d71b3fdf56f1e3ff471672c
Joshua Germon <joshgermon@gmail.com>
2022-08-15 20:51:44 +1000

    Added prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import fonts from 'astro-fonts-next';
 import svelte from "@astrojs/svelte";
 
+import prefetch from "@astrojs/prefetch";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [fonts({
     url: 'https://fonts.googleapis.com/css2?family=Manrope&family=Space+Grotesk:wght@500&display=swap'
-  }), svelte()]
+  }), svelte(), prefetch()]
 });
