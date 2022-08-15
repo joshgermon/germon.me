56f5081df152d3a614145d9bdf77297d7dda2413
Joshua Germon <joshgermon@gmail.com>
2022-08-15 20:34:06 +1000

    Added back SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from 'astro/config';
 import fonts from 'astro-fonts-next';
 import svelte from "@astrojs/svelte";
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [fonts({
     url: 'https://fonts.googleapis.com/css2?family=Manrope&family=Space+Grotesk:wght@500&display=swap'
-  }), svelte()]
+  }), svelte()],
+  adapter: netlify(),
+  output: 'server'
 });
