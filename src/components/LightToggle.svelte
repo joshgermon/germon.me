4fa76fda0ceacbf06daa5849165394c8916fcba6
Joshua Germon <joshgermon@gmail.com>
2022-08-15 12:10:51 +1000

    Theme & Light Options

---
+++ src/components/LightToggle.svelte
@@ -0,0 +1,15 @@
+<script>
+    function toggleColorScheme() {
+        if (document.body.classList.contains('light')) {
+            document.body.classList.remove('light');
+            localStorage.setItem('lightMode', false);
+        } else {
+            localStorage.setItem('lightMode', true);
+            document.body.classList.add('light');
+        }
+    }
+</script>
+
+<button class="toggle-color-scheme icon-button" on:click={toggleColorScheme} aria-label="Toggle Color Scheme">
+    <slot />
+</button>
---
+++ src/components/ThemeToggle.svelte
@@ -0,0 +1,42 @@
+<script>
+    import { onMount } from 'svelte';
+	onMount(async () => {
+        const selectedTheme = localStorage.getItem('theme');
+        if(selectedTheme) {
+            console.log(selectedTheme)
+            document.querySelector(`[data-theme="${selectedTheme}"]`).dataset.active = true;
+        }else {
+            document.querySelector(`[data-theme="default"]`).dataset.active = true;
+        }
+	});
+
+    function handleThemeSwitch(e) {
+        if(e.target.classList.contains('theme-option')) {
+            document.body.classList.remove(e.currentTarget.querySelector('[data-active="true"]').dataset.theme);
+            delete e.currentTarget.querySelector('[data-active="true"]').dataset.active;
+            localStorage.setItem('theme', e.target.dataset.theme);
+            document.body.classList.add(e.target.dataset.theme);
+            e.target.dataset.active = true;
+        }
+    }
+</script>
+
+<div class="theme-selector" on:click={handleThemeSwitch}>
+    <div class="theme-option" data-theme="default">Default</div>
+    <div class="theme-option" data-theme="warm">Warm</div>
+</div>
+
+<style lang="scss">
+    .theme-selector {
+        display: flex;
+        border-radius: 4px;
+        border: 1px solid var(--border-color);
+        .theme-option {
+            cursor: pointer;
+            border-radius: 4px;
+            padding: 0 0.75rem;
+            color: rgba(var(--hover-color), 0.5);
+            transition: background-color 300ms ease;
+        }
+    }
+</style>
