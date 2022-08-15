823ac25112fd5027dc102e40a7ed4ba841eb6271
Joshua Germon <joshgermon@gmail.com>
2022-08-15 20:42:07 +1000

    Removed SSR --again

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,9 @@ import { defineConfig } from 'astro/config';
 import fonts from 'astro-fonts-next';
 import svelte from "@astrojs/svelte";
 
-import netlify from "@astrojs/netlify/functions";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [fonts({
     url: 'https://fonts.googleapis.com/css2?family=Manrope&family=Space+Grotesk:wght@500&display=swap'
-  }), svelte()],
-  adapter: netlify(),
-  output: 'server'
+  }), svelte()]
 });
