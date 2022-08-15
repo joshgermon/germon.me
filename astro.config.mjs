25145f3381d8dd445f90c48faa49c374875beb73
Joshua Germon <joshgermon@gmail.com>
2022-08-15 17:37:52 +1000

    Removed optimise fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
 import { defineConfig } from 'astro/config';
-import fonts from 'astro-fonts-next';
 import netlify from '@astrojs/netlify/edge-functions';
-
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [fonts({
-    url: 'https://fonts.googleapis.com/css2?family=Manrope&family=Space+Grotesk:wght@500&display=swap'
-  }), svelte()],
-  experimental: {
-    integrations: true
-  },
-  adapter: netlify()
+  integrations: [svelte()],
+  adapter: netlify(),
+  output: 'server'
 });
