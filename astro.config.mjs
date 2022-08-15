2112dac83b4d5d6e1605961ef0813e53101e82f6
Joshua Germon <joshgermon@gmail.com>
2022-08-15 23:13:40 +1000

    Spotify Now Playing API + SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from 'astro/config';
 import fonts from 'astro-fonts-next';
 import svelte from "@astrojs/svelte";
-
 import prefetch from "@astrojs/prefetch";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [fonts({
     url: 'https://fonts.googleapis.com/css2?family=Manrope&family=Space+Grotesk:wght@500&display=swap'
-  }), svelte(), prefetch()]
+  }), svelte(), prefetch({
+    throttle: 5
+  })],
+  adapter: netlify(),
+  output: 'server'
 });
---
+++ utils/spotify.js
@@ -0,0 +1,23 @@
+
+const client_id = import.meta.env.SPOTIFY_CLIENT_ID;
+const client_secret = import.meta.env.SPOTIFY_CLIENT_SECRET;
+const refresh_token = import.meta.env.SPOTIFY_REFRESH_TOKEN;
+
+const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
+const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
+
+export const getAccessToken = async () => {
+  const response = await fetch(TOKEN_ENDPOINT, {
+    method: 'POST',
+    headers: {
+      Authorization: `Basic ${basic}`,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: new URLSearchParams({
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    })
+  });
+
+  return response.json();
+};
