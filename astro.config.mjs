ace06b48de89be7e5cc53c6bf6fc8005f92ae7cb
Joshua <joshgermon@gmail.com>
2025-02-22 12:37:58 +1100

    Update description & add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,20 @@ import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
 import vercel from "@astrojs/vercel/serverless";
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   integrations: [svelte()],
   output: 'server',
+
   adapter: vercel({
     webAnalytics: {
       enabled: true,
     },
     maxDuration: 8,
   }),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
